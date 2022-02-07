269f9802574217246d5318cc933b8a00cb1b9c99
Allie <huaruhsiao.b@gmail.com>
2022-02-08 01:38:03 +0800

    [ADD] backend initial folders & files

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+require('dotenv').config();
+const path = require('path');
+const cors = require('cors');
+
+let app = express();
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    credentials: true,
+  })
+);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const expressSession = require('express-session');
+let FileStore = require('session-file-store')(expressSession);
+app.use(
+  expressSession({
+    store: new FileStore({
+      path: path.join(__dirname, '..', 'sessions'),
+    }),
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(express.static(path.join(__dirname, 'assets')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+let memberRouter = require('./routers/member');
+app.use('/api/member', memberRouter);
+
+let authRouter = require('./routers/auth');
+app.use('/api/auth', authRouter);
+
+app.use((req, res, next) => {
+  res.status(404).send('404 not found');
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send('server錯誤');
+});
+
+const port = process.env.SERVER_PORT || 3002;
+app.listen(port, () => {
+  console.log(`server running at port ${port}`);
+});
---
+++ utils/database.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+let pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  // 加上連線數限制
+  connectionLimit: 10,
+  // 讓mySQL2不要把date轉成js物件
+  dateStrings: true,
+});
+module.exports = pool.promise();
