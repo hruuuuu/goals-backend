a411f79209ab9befb674dacf47f5d432e0057af4
nick840290 <nick840290@gmail.com>
2022-02-18 16:42:54 +0800

    [add] order router

--- routers/order.js
+++ routers/order.js
@@ -2,12 +2,24 @@ const express = require("express");
 const router = express.Router();
 const connection = require("../utils/database");
 
-router.get("/", async (req, res, next) => {
-  let [data] = await connection.execute("SELECT * FROM goals.order_details");
+router.post("/", async (req, res, next) => {
+  // let [data] = await connection.execute("SELECT * FROM goals.order_details");
   //   console.log(setData);
+  console.log(req.body);
+
+  let [result] = await connection.execute(
+    "UPDATE goals.member SET username=?, email=? WHERE id=?",
+    [req.body.name, req.body.email, 1]
+  );
+
+  let [result1] = await connection.execute(
+    "UPDATE order_details SET address=?, tel=? WHERE id=?",
+    [req.body.address, req.body.tel, 1]
+  );
 
   //   console.log(data[0].tel);
-  res.json(data);
+  res.json({ message: "ok" });
+  // res("hello");
 });
 
 module.exports = router;
