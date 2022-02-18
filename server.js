3966aedcca82feff4ae85818e70f1fd4b153cbd2
Chris9271 <chrisweng71@outlook.com>
2022-02-18 16:19:02 +0800

    [ADD] related to user login's router and function

--- server.js
+++ server.js
@@ -37,6 +37,9 @@ app.use('/api/member', memberRouter);
 let authRouter = require('./routers/auth');
 app.use('/api/auth', authRouter);
 
+let verifyRouter = require('./routers/verify');
+app.use('/api/verify', verifyRouter);
+
 app.use((req, res, next) => {
   res.status(404).send('404 not found');
 });
