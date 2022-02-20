f7a88666508076fd2ddf951ba2fcfbc9afbcbf87
nick840290 <nick840290@gmail.com>
2022-02-20 17:43:36 +0800

    [edit]

--- routers/order.js
+++ routers/order.js
@@ -2,24 +2,15 @@ const express = require("express");
 const router = express.Router();
 const connection = require("../utils/database");
 
-router.post("/", async (req, res, next) => {
-  // let [data] = await connection.execute("SELECT * FROM goals.order_details");
-  //   console.log(setData);
-  console.log(req.body);
+router.get("/", async (req, res, next) => {
+  let [data] = await connection.execute(
+    "SELECT * FROM goals.order_details INNER JOIN payment_status on payment_status_id = payment_status.id WHERE goals.order_details.id = ?",
+    [1],
 
-  let [result] = await connection.execute(
-    "UPDATE goals.member SET username=?, email=? WHERE id=?",
-    [req.body.name, req.body.email, 1]
+    "SELECT * FROM goals.order_details LEFT JOIN order_status on order_status_id = order_status.id WHERE goals.order_details.id = ?"[1]
   );
 
-  let [result1] = await connection.execute(
-    "UPDATE order_details SET address=?, tel=? WHERE id=?",
-    [req.body.address, req.body.tel, 1]
-  );
-
-  //   console.log(data[0].tel);
-  res.json({ message: "ok" });
-  // res("hello");
+  res.json(data);
 });
 
 module.exports = router;
