44638db61859f97ee0ed47d61c5dbc5fddc4ae39
nick840290 <nick840290@gmail.com>
2022-02-20 22:41:20 +0800

    [add] order feature

--- routers/order.js
+++ routers/order.js
@@ -4,10 +4,8 @@ const connection = require("../utils/database");
 
 router.get("/", async (req, res, next) => {
   let [data] = await connection.execute(
-    "SELECT * FROM goals.order_details INNER JOIN payment_status on payment_status_id = payment_status.id WHERE goals.order_details.id = ?",
-    [1],
-
-    "SELECT * FROM goals.order_details LEFT JOIN order_status on order_status_id = order_status.id WHERE goals.order_details.id = ?"[1]
+    "SELECT goals.order_details.id ,create_at , payment_status , order_status, price , amount FROM goals.order_details INNER JOIN payment_status on payment_status_id = payment_status.id INNER JOIN order_status on order_status_id = order_status.id INNER JOIN order_items ON order_items.order_id = goals.order_details.id INNER JOIN product ON product.id = order_items.product_id WHERE goals.order_details.member_id = ?",
+    [1]
   );
 
   res.json(data);
