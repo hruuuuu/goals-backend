6928cd543e51af3357a9c24db815a7cd35ebcc4d
meizi07 <rainy2525777@gmail.com>
2022-02-22 15:41:32 +0800

    no message

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
-const express = require('express');
-require('dotenv').config();
-const path = require('path');
-const cors = require('cors');
-const passport = require('passport');
-=======
 const express = require("express");
 require("dotenv").config();
 const path = require("path");
 const cors = require("cors");
 const passport = require("passport");
->>>>>>> 3c0134f95463aac585b153d02acf31ba7b35d480
 
 let app = express();
 
@@ -42,35 +34,17 @@ app.use(passport.session());
 app.use(express.static(path.join(__dirname, "assets")));
 app.use("/public", express.static(path.join(__dirname, "public")));
 
-<<<<<<< HEAD
-let blogRouter = require('./routers/blog');
-app.use('/api/blog', blogRouter);
-
-let memberRouter = require('./routers/member');
-app.use('/api/member', memberRouter);
-=======
 let blogRouter = require("./routers/blog");
 app.use("/api/blog", blogRouter);
 
 let memberRouter = require("./routers/member");
 app.use("/api/member", memberRouter);
->>>>>>> 3c0134f95463aac585b153d02acf31ba7b35d480
 
 let authRouter = require("./routers/auth");
 app.use("/api/auth", authRouter);
 
-<<<<<<< HEAD
-let orderRouter = require("./routers/order");
-app.use("/api/order", orderRouter);
-let verifyRouter = require('./routers/verify');
-app.use('/api/verify', verifyRouter);
-
-let socialRouter = require('./routers/social');
-app.use('/api/social', socialRouter);
-=======
 let verifyRouter = require("./routers/verify");
 app.use("/api/verify", verifyRouter);
->>>>>>> 3c0134f95463aac585b153d02acf31ba7b35d480
 
 let socialRouter = require("./routers/social");
 app.use("/api/social", socialRouter);
@@ -82,24 +56,12 @@ app.use("/api/order", orderRouter);
 
 let couponRouter = require("./routers/coupon");
 app.use("/api/coupon", couponRouter);
-<<<<<<< HEAD
-
-let cartRouter = require('./routers/cart');
-app.use('/api/cart', cartRouter);
- 
-let favRouter = require('./routers/fav');
-app.use('/api/fav', favRouter);
-
-let activityRouter = require('./routers/activity');
-app.use('/api/activity', activityRouter);
-=======
 
 let favRouter = require("./routers/fav");
 app.use("/api/fav", favRouter);
 
 let activityRouter = require("./routers/activity");
 app.use("/api/activity", activityRouter);
->>>>>>> 3c0134f95463aac585b153d02acf31ba7b35d480
 
 app.use((req, res, next) => {
   res.status(404).send("404 not found");
