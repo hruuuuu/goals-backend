64a7e2a41d54b9f74dfcc035f7286104461d2c13
nick840290 <nick840290@gmail.com>
2022-02-23 16:05:42 +0800

    [edit]severral feature

--- routers/member.js
+++ routers/member.js
@@ -2,17 +2,15 @@ const express = require("express");
 const router = express.Router();
 const connection = require("../utils/database");
 
-router.get("/getprofile", async (req, res, next) => {
+router.post("/getprofile", async (req, res, next) => {
   let [data] = await connection.execute(
     "SELECT * FROM goals.member WHERE id=?",
-    [1]
+    [req.body.id]
   );
   res.json(data);
 });
 
 router.post("/editprofile", async (req, res, next) => {
-  console.log(req.body);
-
   let [result] = await connection.execute(
     "UPDATE goals.member SET username=?, email=? ,default_address=?, default_tel=? WHERE id=?",
     [
@@ -20,7 +18,7 @@ router.post("/editprofile", async (req, res, next) => {
       req.body.email,
       req.body.default_address,
       req.body.default_tel,
-      1,
+      req.body.id,
     ]
   );
 
@@ -28,11 +26,9 @@ router.post("/editprofile", async (req, res, next) => {
 });
 
 router.post("/editpassword", async (req, res, next) => {
-  console.log(req.body);
-
   let [result] = await connection.execute(
     "UPDATE goals.member SET password=? WHERE id=?",
-    [req.body.newpassword, 1]
+    [req.body.newpassword, req.body.id]
   );
 
   res.json({ message: "ok" });
--- routers/order.js
+++ routers/order.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const connection = require("../utils/database");
 
-router.get("/", async (req, res, next) => {
+router.post("/", async (req, res, next) => {
   let [data] = await connection.execute(
     "SELECT goals.order_details.id ,create_at , payment_status , order_status, price , amount FROM goals.order_details INNER JOIN payment_status on payment_status_id = payment_status.id INNER JOIN order_status on order_status_id = order_status.id INNER JOIN order_items ON order_items.order_id = goals.order_details.id INNER JOIN product ON product.id = order_items.product_id WHERE goals.order_details.member_id = ?",
-    [1]
+    [req.body.id]
   );
 
   res.json(data);
