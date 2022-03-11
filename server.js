ed66256d1dc86f7e5de810ab0c036b22d8938330
Allie <huaruhsiao.b@gmail.com>
2022-03-11 18:59:14 +0800

    [UPDATE] koa

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const passport = require('passport');
 const socketio = require('./utils/socketio');
 
-const app = express();
+const app = new Koa();
 const staticPath = './';
 
 app.use(
